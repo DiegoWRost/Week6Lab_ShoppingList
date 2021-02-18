26abe0f62f6e3df21ad48106bf315a021df4f3c5
DiegoWR <806509@SAIT225715IT.ACDM.DS.SAIT.CA>
2021-02-18 09:22:55 -0700

    Week 6 Lab Login Login and redirection working

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -17,11 +18,44 @@ public class ShoppingListServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         
+        HttpSession session = request.getSession();
+        String action = request.getParameter("action");
+        String username = (String) session.getAttribute("username");
+        
+        String registerURL = "/WEB-INF/register.jsp";
+        String shoppingURL = "/WEB-INF/shoppinglist.jsp";
+            
+        if (action != null && action.equals("logout")) {
+           session.invalidate();
+           getServletContext().getRequestDispatcher(registerURL).forward(request, response);
+        }
+        
+        if (username != null && !username.isEmpty()) {
+            session.setAttribute("username", username);
+            getServletContext().getRequestDispatcher(shoppingURL).forward(request, response);
+        } else {
+            getServletContext().getRequestDispatcher(registerURL).forward(request, response);
+        }
     } 
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         
+        HttpSession session = request.getSession();
+        String userField = request.getParameter("username");
+        
+        String registerURL = "/WEB-INF/register.jsp";
+        String shoppingURL = "/WEB-INF/shoppinglist.jsp";
+        String action = request.getParameter("action");
+        
+        if (userField != null && !userField.isEmpty() && action.equals("register")) {
+            session.setAttribute("username", userField);
+            getServletContext().getRequestDispatcher(shoppingURL).forward(request, response);
+        } else {
+            request.setAttribute("message", "Error: Username cannot be empty");
+            request.setAttribute("userField", userField);
+            getServletContext().getRequestDispatcher(registerURL).forward(request, response);
+        }
     }
 }
--- web/WEB-INF/register.jsp
+++ web/WEB-INF/register.jsp
@@ -9,9 +9,17 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Register</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Shopping List</h1>
+        
+        <form action="ShoppingList" method="post">
+            Username: <input type="text" name="username">
+            <input type="hidden" name="action" value="register">
+            <input type="submit" value="Register">
+        </form>
+            
+        <p>${message}</p>
     </body>
 </html>
